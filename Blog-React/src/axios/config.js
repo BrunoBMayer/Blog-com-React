c54a85acb73a7d7f5d44d66fe7bb0fdea0d9f43d
Bruno Mayer <brunobrmayer@gmail.com>
2026-05-28 12:29:29 -0300

    feat: JSX e CSS de "NewPost"

--- Blog-React/src/axios/config.js
+++ Blog-React/src/axios/config.js
@@ -1,10 +1,11 @@
+//Centraliza a configuração do Axios para não precisar repetir a URL inteira da API o tempo todo 
 import axios from "axios"; 
 
-const blogFetch = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com",
-    headers:{
-        "Content-Type": "application/json",
-    },
+const blogFetch = axios.create({ //Instância personalizada do Axios (“Vou criar uma versão do Axios já configurada para a API do blog.”)
+  baseURL: "https://jsonplaceholder.typicode.com", // URL base da API, evita repetir o endereço completo
+  headers: {
+    "Content-Type": "application/json", // informa que os dados serão trabalhados em formato JSON
+  },
 });
 
 export default blogFetch; 
--- Blog-React/src/routes/NewPost.jsx
+++ Blog-React/src/routes/NewPost.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import "./NewPost.css"; 
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div className="new-post"> {/* Container principal da página de criação de post */}
+      <h2>Inserir novo Post:</h2>
+
+      <form> 
+        <div className="form-control"> {/* Agrupa label + input para facilitar a estilização */}
+          <label htmlFor="title">Título:</label> {/* htmlFor conecta o label ao input com id="title" */}
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Digite o título"
+          />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label> {/* Conecta o label ao textarea com id="body" */}
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Digite o conteúdo"
+          ></textarea>
+        </div>
+
+        <input 
+          type="submit" 
+          value="Criar Post" 
+          className="btn" 
+        /> {/* Botão que envia o formulário */}
+      </form>
+    </div>
   )
 }
 
