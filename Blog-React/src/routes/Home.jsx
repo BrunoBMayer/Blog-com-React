ecba04eb4e3a7802393b67f3249eaec9ba37b744
Bruno Mayer <brunobrmayer@gmail.com>
2026-05-30 17:29:39 -0300

    feat: adicionando uma aba de pesquisa

--- Blog-React/src/routes/Home.jsx
+++ Blog-React/src/routes/Home.jsx
@@ -5,6 +5,7 @@ import "./Home.css";
 
 const Home = () => {
   const [posts, setPosts] = useState([]); // Estado que começa vazio e depois guarda a lista de posts da API
+  const [search, setSearch] = useState(""); //guardar oq o usuário digitar na barra d pesquisa
 
   const getPosts = async () => { // Função assíncrona para buscar os posts na API
     try {
@@ -24,26 +25,46 @@ const Home = () => {
     getPosts(); // Chama a função quando o componente Home carregar
   }, []); // Array vazio faz o useEffect rodar apenas uma vez
 
+// Filtra os posts pelo título digitado na barra de pesquisa
+// toLowerCase() evita diferença entre maiúsculas e minúsculas
+// includes() verifica se o título contém o texto pesquisado
+const filteredPosts = posts
+  .filter((post) =>
+    post.title.toLowerCase().includes(search.toLowerCase())
+  )
+  .slice(0, 10);
+
   return (
     <div className="home">
       <h1>Últimos posts</h1>
 
+      <div className="search-container">
+        <input
+         type="text"
+         placeholder="Pesquisar posts pelo título..."
+         value={search}
+         onChange={(e) => setSearch(e.target.value)}  // Ao digitar nesse input, ele pega o valor atual digitado e salva dentro do estado search.” (onChange é acionado toda vez q o input muda e o "e" guarda as info do q aconteceu)
+          />
+      </div>
+
       {/* Se ainda não tiver posts, mostra "Carregando...".
           Quando os posts chegarem da API, renderiza a lista com map */}
-      {posts.length === 0 ? (
-        <p>Carregando...</p>
-      ) : (
-        posts.slice(0, 10).map((post) => ( // Percorre o array de posts e cria um bloco para cada post (limitando aos 10 primeiros (slice pega uma parte do array))
-          <div className="post" key={post.id}> {/* key ajuda o React a identificar cada item da lista */}
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
+        {posts.length === 0 ? (
+      <p>Carregando...</p>
+    ) : filteredPosts.length === 0 ? (
+      <p>Nenhum post encontrado.</p>
+    ) : (
+      filteredPosts.map((post) => (
+        <div className="post" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
 
-            <Link to={`/posts/${post.id}`} className="btn"> {/* Link dinâmico para a página específica do post */}
-              Ler mais
-            </Link>
-          </div>
-        ))
-      )}
+          <Link to={`/posts/${post.id}`} className="btn">
+            Ler mais
+          </Link>
+        </div>
+      ))
+    )}
     </div>
   );
 };
