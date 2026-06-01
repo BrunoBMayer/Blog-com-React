1755c6844c93d5fddffdc2d118b4e8e5ac0a1b19
Bruno Mayer <brunobrmayer@gmail.com>
2026-06-01 12:19:18 -0300

    feat: replace JSONPlaceholder with local JSON Server

--- Blog-React/src/axios/config.js
+++ Blog-React/src/axios/config.js
@@ -1,11 +1,14 @@
-//Centraliza a configuração do Axios para não precisar repetir a URL inteira da API o tempo todo 
-import axios from "axios"; 
+// Centraliza a configuração do Axios para não precisar repetir a URL inteira da API o tempo todo
+import axios from "axios";
 
-const blogFetch = axios.create({ //Instância personalizada do Axios (“Vou criar uma versão do Axios já configurada para a API do blog.”)
-  baseURL: "https://jsonplaceholder.typicode.com", // URL base da API, evita repetir o endereço completo
+const blogFetch = axios.create({
+  // URL base da API local criada com json-server
+  baseURL: "http://localhost:3000",
+
   headers: {
-    "Content-Type": "application/json", // informa que os dados serão trabalhados em formato JSON
+    // Informa que os dados enviados/recebidos serão em formato JSON
+    "Content-Type": "application/json",
   },
 });
 
-export default blogFetch; 
+export default blogFetch;
